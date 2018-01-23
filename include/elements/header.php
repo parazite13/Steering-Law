1a87073dbb868d69ae0b9af34d92dc8d5bea31d9
Vincent <vincent.paoli.1@etu.univ-amu.fr>
2018-01-23 16:38:44 +0100

    C'est trop beau. Les boutons.

--- include/elements/header.php
+++ include/elements/header.php
@@ -1,8 +1,28 @@
 <nav class="navbar navbar-light bg-faded">
 	<?php if(isAdmin()): ?>
-		<form action="" method="post">
-			<button type="submit" value="true" class="btn btn-secondary" name="disconnect" role="button">Se déconnecter</button>
-		</form>
+		<div class="btn-toolbar justify-content-between" role="toolbar">
+			<?php if(getCurrentUrl() == ABSURL): ?>
+				<a href="<?=ABSURL?>admin/">
+					<button type="submit" value="true" class="btn btn-secondary" role="button">
+						Éspace Admin
+					</button>
+				</a>
+			<?php endif; ?>
+
+			<?php if(getCurrentUrl() == ABSURL . "admin/"): ?>
+				<a href="<?=ABSURL?>">
+					<button type="submit" value="true" class="btn btn-secondary d-inline-block" role="button">
+						Faire l'expérience
+					</button>
+				</a>
+			<?php endif; ?>
+
+			<form action="" method="post">
+				<button type="submit" value="true" class="btn btn-secondary d-inline-block" name="disconnect" role="button">
+					Se déconnecter
+				</button>
+			</form>
+		</div>
 	<?php else: ?>
 		<a href="admin/">
 			<button id="btn-admin" class="btn btn-secondary" role="button">
